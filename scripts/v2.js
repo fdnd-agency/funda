7e8cc515b05335611b4f4812e136eae9d2757528
yujing <medischdossier03@gmail.com>
2024-01-18 15:27:48 +0100

    toevoegen code

--- scripts/v2.js
+++ scripts/v2.js
@@ -1,6 +1,8 @@
 ShowHamburgerMenu();
 ShowMoreTextDescription();
 ShowMoreTextCharacterize();
+showtelephonenumber();
+clickBroker();
 function ShowHamburgerMenu () {
     let buttonicon = document.querySelector('.hamburger-icon');
     buttonicon.addEventListener('click', () => {
@@ -27,21 +29,44 @@ function ShowMoreTextCharacterize(){
 
     })
 }
+function showtelephonenumber(){
+    let telephonnumber = document.querySelector('.telephonumber');
+    let phoneNumber = document.querySelector('.hide-telehponenumber');
+
+    telephonnumber.addEventListener("click", () => {
+        phoneNumber.classList.toggle('open-telephonenumber');
+        telephonnumber.scrollIntoView({behavior: 'smooth'});
+
+
+    });
+
+}
+function clickBroker(){
+    let brokerdiv = document.querySelector('.broker-add');
+    let clickContact = document.querySelector('.contact-broker');
+    let mql = window.matchMedia("(max-width: 768px)");
+    clickContact.addEventListener("click", () => {
+        if(mql){
+            brokerdiv.scrollIntoView({behavior: 'smooth'});
+        }
+    });
+
+}
 
 // refactor code for less duplicate code not finish
-function opentoggle(hamburger,description,characterize){
-    let buttonhamber = document.querySelector(hamburger);
-    let buttondescription =document.querySelector(description);
-    let buttoncharaterize = document.querySelector(characterize);
-
-    buttonhamber.addEventListener('click', () => {
-        buttonhamber.classList.toggle('menu-ul-open');
-    })
-    buttondescription.addEventListener('click', () => {
-        buttondescription.classList.toggle('open-description');
-    })
-    buttoncharaterize.addEventListener('click', () => {
-        buttoncharaterize.classList.toggle('open-characterize');
-    });
-}
+// function opentoggle(hamburger,description,characterize){
+//     let buttonhamber = document.querySelector(hamburger);
+//     let buttondescription =document.querySelector(description);
+//     let buttoncharaterize = document.querySelector(characterize);
+//
+//     buttonhamber.addEventListener('click', () => {
+//         buttonhamber.classList.toggle('menu-ul-open');
+//     })
+//     buttondescription.addEventListener('click', () => {
+//         buttondescription.classList.toggle('open-description');
+//     })
+//     buttoncharaterize.addEventListener('click', () => {
+//         buttoncharaterize.classList.toggle('open-characterize');
+//     });
+// }
 // opentoggle('.hamburger-icon','.full-description','.button-show-charactize');
